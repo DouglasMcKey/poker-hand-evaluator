f99e3359f9768e8808d9bb025c3d08a3e8cc168e
Douglas McKey <douglas.mckey@thegocompany.io>
2025-12-20 15:21:34 +0200

    Front-end updates.
    The majority of the front end work is complete. Includes:
    - select inputs filter out cards already selected. Eliminating the need for additional font end validation on card selections.
    - front-end layout is basic but improved.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {SharedModule} from "./shared/shared-module";
+import {FormsModule} from "@angular/forms";
 
 @Component({
     selector: 'app-root',
-    imports: [SharedModule],
+    imports: [SharedModule, FormsModule],
     templateUrl: './app.html',
     styleUrl: './app.css'
 })
 export class App implements OnInit {
-    // The master deck to  manage the available cards.
     masterDeck: any[] = [];
     selectedHand: any[] = [null, null, null, null, null]
+    evaluationResult = "";
 
     ngOnInit() {
         this.generateDeck();
@@ -18,8 +19,8 @@ export class App implements OnInit {
 
     generateDeck() {
         /**
-         * Build a regular deck of cards and push them to the master deck.
-         * These will be used to create the select options for the user.
+         * Build a regular deck of cards and push them to the masterDeck,
+         * which will be used to create the select options for the form.
          */
         const cardValues = [
             [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10],
@@ -29,10 +30,10 @@ export class App implements OnInit {
         // The offset helps to distinguish between suites and should aid in
         // determining potential hand rankings later on.
         const suites = [
-            { name: "Hearts", offset: 100 },
-            { name: "Diamonds", offset: 200 },
-            { name: "Clubs", offset: 300 },
-            { name: "Spades", offset: 400 }
+            {name: "Hearts", offset: 100},
+            {name: "Diamonds", offset: 200},
+            {name: "Clubs", offset: 300},
+            {name: "Spades", offset: 400}
         ]
 
         let idCounter = 1;
@@ -46,5 +47,29 @@ export class App implements OnInit {
                 })
             })
         })
+    };
+
+    getAvailableCards() {
+        /**
+         * Returns a filtered deck of cards by filtering the masterDeck and
+         * excludes the cards already picked by hand slots.
+         */
+        return this.masterDeck.filter(card => {
+            // Check if card is already selected in any of the other hand slots.
+            const isPicked = this.selectedHand.some(selected => selected?.id === card.id);
+
+            // Return cards not picked.
+            return !isPicked;
+        });
+    }
+
+    onsubmit() {
+        console.log("Hand to evaluate:", this.selectedHand);
+        this.evaluationResult = "Return the result here!";
+    }
+
+    resetForm() {
+        this.selectedHand = [null, null, null, null, null];
+        this.evaluationResult = ""
     }
 }
